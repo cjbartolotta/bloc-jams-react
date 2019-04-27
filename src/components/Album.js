1bfd74b11262f23497f023e1deb48bcf8c54c306
Chris Bartolotta <chrisjbartolotta@gmail.com>
2019-04-26 20:19:15 -0400

    Checkpoint 7 complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -48,6 +48,14 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   handleMouseHover(index) {
     this.setState({hoveredIndex: index})
   }
@@ -108,7 +116,12 @@ class Album extends Component {
               }
             </tbody>
           </table>
-          <PlayerBar isPlaying= {this.state.isPlaying} currentSong= {this.state.currentSong} />
+          <PlayerBar
+            isPlaying= {this.state.isPlaying}
+            currentSong= {this.state.currentSong}
+            handleSongClick= {() => this.handleSongClick(this.state.currentSong)}
+            handlePrevClick= {() => this.handlePrevClick()}
+           />
       </section>
     );
   }
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -5,27 +5,29 @@ class PlayerBar extends Component {
     return (
       <section className="player-bar">
         <section id="buttons">
-          <button id="previous">
-            <span className="ion-skip-backward"></span>
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="ion-md-skip-backward"></span>
           </button>
-          <button id="play-pause">
-            <span  className={this.props.isPlaying ? "ion-md-pause" : "ion-md-play"}></span>
+          <button id="play-pause" onClick={this.props.handleSongClick}>
+            <span className="ion-md-play"></span>
+            <span className="ion-md-pause"></span>
           </button>
           <button id="next">
-            <span className="ion-skip-forward"></span>
+            <span className="ion-md-skip-forward"></span>
           </button>
-        </section>  
+        </section>
+        <section id="time-control">
+          <div className="current-time">–:––</div>
+          <input type="range" className="seek-bar" value="0" />
+          <div className="total-time">–:––</div>
+        </section>
+        <section id="volume-control">
+          <div className="icon ion-md-volume-low"></div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="icon ion-md-volume-high"></div>
+        </section>
       </section>
-      <section id="time-control">
-        <div className="current-time">-:--</div>
-        <input type="range" className="seek-bar" value="0" />
-        <div className="total-time">-:--</div>
-      </section>
-      <section id="volume-control">
-        <div className="icon ion-volume-low"></div>
-        <input type="range" className="seek-bar" value="80" />
-        <div className="icon ion-volume-high"></div>
-      </section>
+
 
     );
   }
