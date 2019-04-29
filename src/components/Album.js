8d36156a25ee7f43b0793bfc9eb6f30fe69fe040
Chris Bartolotta <chrisjbartolotta@gmail.com>
2019-04-29 17:52:50 -0400

    Assignment 7 revision in progress

--- src/components/Album.js
+++ src/components/Album.js
@@ -49,8 +49,15 @@ class Album extends Component {
   }
 
   handleNextClick() {
+    const nextOrStartOver = () => {
+      if ((currentIndex + 1) > this.state.album.songs.length) {
+        return 0
+      } else {
+        return (currentIndex + 1)
+      }
+    }
     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-    const newIndex = Math.min(this.state.album.songs.length, currentIndex + 1);
+    const newIndex = nextOrStartOver;
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
